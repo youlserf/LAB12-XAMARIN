c8ec4b2e1882943a8f9b8d972b18d8aeee74e2ce
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2022-11-15 12:03:15 -0500

    Add project files.

---
+++ MVVMCalculator/MVVMCalculator/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVMCalculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Views.Calculator();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MVVMCalculator/MVVMCalculator/ViewModels/ViewModelCalculator.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVMCalculator.ViewModels
+{
+    public class ViewModelCalculator : ViewModelBase
+    {
+        #region Propiedades
+        int currentState = 1;
+        string mathOperator;
+
+        double firstNumber;
+        public double FirstNumber
+        {
+            get { return firstNumber; }
+            set
+            {
+                if (firstNumber != value)
+                {
+                    firstNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        double secondNumber;
+        public double SecondNumber
+        {
+            get { return secondNumber; }
+            set
+            {
+                if (secondNumber != value)
+                {
+                    secondNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        string result;
+		public string Result
+		{
+			get { return result; }
+			set
+			{
+				if (result != value)
+				{
+					result = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+        #endregion
+        public ICommand OnSelectNumber { protected set; get; }
+        public ICommand OnClear { protected set; get; }
+
+        
+
+        public ViewModelCalculator()
+        {
+            OnClear = new Command(() =>
+            {
+                firstNumber = 0;
+                secondNumber = 0;
+                currentState = 1;
+                this.Result = "0";
+            });
+
+            OnSelectNumber = new Command<string>(
+               execute: (string parameter) =>
+               {
+				   
+				   string pressed = parameter;
+
+				   if (Result == "0" || currentState < 0)
+				   {
+					   Result = "";
+					   if (currentState < 0)
+						   currentState *= -1;
+				   }
+
+				   Result += pressed;
+
+				   double number;
+				   if (double.TryParse(Result, out number))
+				   {
+					   Result = number.ToString("N0");
+					   if (currentState == 1)
+					   {
+						   firstNumber = number;
+					   }
+					   else
+					   {
+						   secondNumber = number;
+					   }
+				   }
+			   });
+
+        }
+
+
+    }
+}
